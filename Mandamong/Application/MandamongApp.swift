1cdb2de5ecb92dca8b4121134f51d9005f965bb1
YunSwain <destap@naver.com>
2025-09-01 22:36:57 +0900

    [Feat]: First Commit

---
+++ Mandamong/Application/MandamongApp.swift
@@ -0,0 +1,17 @@
+//
+//  MandamongApp.swift
+//  Mandamong
+//
+//  Created by Swain Yun on 9/1/25.
+//
+
+import SwiftUI
+
+@main
+struct MandamongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
