babb46d30706793fb9db2bea1764888a4189998a
YunSwain <destap@naver.com>
2025-09-17 00:33:46 +0900

    [Setting] #1 - 초기 세팅 파일 설정

---
+++ Tuist/Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 6.0
+import PackageDescription
+
+#if TUIST
+    import struct ProjectDescription.PackageSettings
+
+    let packageSettings = PackageSettings(
+        // Customize the product types for specific package product
+        // Default is .staticFramework
+        // productTypes: ["Alamofire": .framework,]
+        productTypes: [:]
+    )
+#endif
+
+let package = Package(
+    name: "Projects",
+    dependencies: [
+        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "15.0.0")),
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture.git", .upToNextMajor(from: "1.22.2"))
+    ]
+)
---
+++ Workspace.swift
@@ -0,0 +1,8 @@
+import ProjectDescription
+
+let workspace = Workspace(
+    name: "Mandamong-iOS",
+    projects: [
+        "Projects/**"
+    ]
+)
