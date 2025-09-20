192ad83cfe4c663d17981b18c3f3ed74bba383b7
YunSwain <destap@naver.com>
2025-09-20 14:03:51 +0900

    [Refactor] #12 - 요청 스펙 충족에 사용되는 연관값 DTO로 묶어서 전달

--- Projects/Login/LoginData/Sources/Endpoint.swift
+++ Projects/Login/LoginData/Sources/Endpoint.swift
@@ -9,16 +9,16 @@ import Foundation
 
 enum LoginEndpoint {
     // MARK: - No tokens required
-    case checkNicknameDuplication(nickname: String)
-    case checkEmailDuplication(email: String)
+    case checkNicknameDuplication(CheckNicknameDuplicationDTO.Request)
+    case checkEmailDuplication(CheckEmailDuplicationDTO.Request)
     case requestEmailAuthenticationCode(EmailAuthenticationCodeDTO.Request)
-    case verifyEmail(email: String, code: String)
-    case register(email: String, password: String, nickname: String, image: Data, languageID: String)
+    case verifyEmail(VerifyEmailDTO.Request)
+    case register(RegisterDTO.Request)
     case login(LoginDTO.Request)
     case reissueToken(ReissueTokenDTO.Request)
     
     // MARK: - Tokens required
-    case updateProfile(nickname: String?, image: Data?, password: String?)
+    case updateProfile(UpdateProfileDTO.Request)
     case validatePassword(ValidatePasswordDTO.Request)
     case resetPassword(ResetPasswordDTO.Request)
     case unregister
