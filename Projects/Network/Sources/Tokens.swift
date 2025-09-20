aa200a778402e88b3312e3c67d478c7f19078a4b
YunSwain <destap@naver.com>
2025-09-20 18:31:18 +0900

    [Refactor] #19 - Access/Refresh Token 만료 여부 판단 로직 수정 (Clock Skew 등)

--- Projects/Network/Sources/Tokens.swift
+++ Projects/Network/Sources/Tokens.swift
@@ -8,15 +8,15 @@
 import Foundation
 
 struct Tokens: Codable {
-    private static let accessTokenValidityDuration: TimeInterval = 60 * 10
-    
-    let accessToken: String     // 유효기간: 10분
-    let refreshToken: String    // 유효기간: 30일
-    let createdAt: Date
-    var isExpired: Bool {
-        let expirationDate = createdAt.addingTimeInterval(Self.accessTokenValidityDuration)
-        return .now > expirationDate
+    private struct Constant {
+        static let accessTokenValidityDuration: TimeInterval = 60 * 10              // 유효기간: 10분
+        static let refreshTokenValidityDuration: TimeInterval = 60 * 60 * 24 * 30   // 유효기간: 30일
+        static let threshold: TimeInterval = 60 * 3                                 // Clock skew: 3분
     }
+    
+    let accessToken: String
+    let refreshToken: String
+    let createdAt: Date
     
     init(
         accessToken: String,
@@ -26,5 +26,29 @@ struct Tokens: Codable {
         self.accessToken = accessToken
         self.refreshToken = refreshToken
         self.createdAt = createdAt
+    }
+    
+    /// 액세스 토큰의 만료 여부를 확인합니다.
+    /// - Parameters:
+    ///     - date: 기준 시각
+    ///
+    /// - Note:
+    ///     `date` 파라미터의 기본값은 현재 시각(`Date.now`) 입니다.
+    func isAccessTokenExpired(_ date: Date = .now) -> Bool {
+        let expirationDate = createdAt.addingTimeInterval(Constant.accessTokenValidityDuration)
+        let safeExpirationDate = expirationDate.addingTimeInterval(-Constant.threshold)
+        return date > safeExpirationDate
+    }
+    
+    /// 리프레시 토큰의 만료 여부를 확인합니다.
+    /// - Parameters:
+    ///     - date: 기준 시각
+    ///
+    /// - Note:
+    ///     `date` 파라미터의 기본값은 현재 시각(`Date.now`) 입니다.
+    func isRefreshTokenExpired(_ date: Date = .now) -> Bool {
+        let expirationDate = createdAt.addingTimeInterval(Constant.refreshTokenValidityDuration)
+        let safeExpirationDate = expirationDate.addingTimeInterval(-Constant.threshold)
+        return date > safeExpirationDate
     }
 }
