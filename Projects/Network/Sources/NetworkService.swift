d7020733c08444fc1b274203e1448040db8f83fc
JinUng41 <ingjwjw@naver.com>
2025-09-22 01:35:36 +0900

    [Feat] #21 - NetworkService와 프로토콜 구현
    - 리턴형은 ResponseDTO<T>
    - NetworkError는 네트워크 통신 과정에서의 오류, ResponseDTO의 오류는 통신 후 결과물에 대한 오류를 다루는 것으로 이해했기 때문에 외부에서 다뤄져야 한다고 생각함.

---
+++ Projects/Network/Sources/NetworkService.swift
@@ -0,0 +1,114 @@
+//
+//  NetworkService.swift
+//  Network
+//
+//  Created by 김진웅 on 9/22/25.
+//
+
+import Foundation
+import Alamofire
+
+// MARK: - NetworkServiceProtocol
+protocol NetworkServiceProtocol {
+    associatedtype Endpoint: APIEndpoint
+    
+    func request(_ endpoint: Endpoint) async throws(NetworkError)
+    func request<T: Decodable>(_ endpoint: Endpoint) async throws(NetworkError) -> ResponseDTO<T>
+}
+
+// MARK: - NetworkService
+final class NetworkService<Endpoint: APIEndpoint>: NetworkServiceProtocol {
+    private let session: Session
+    private let decoder = JSONDecoder()
+    
+    init(interceptor: (any RequestInterceptor)? = nil) {
+        self.session = Session(
+            interceptor: interceptor,
+            eventMonitors: [NetworkLogger()]
+        )
+    }
+    
+    func request(_ endpoint: Endpoint) async throws(NetworkError) {
+        do {
+            _ = try await session.request(endpoint)
+                .validate()
+                .serializingData()
+                .value
+        } catch let error as AFError {
+            throw mapError(error)
+        } catch {
+            throw NetworkError.unknown(underlyingError: error)
+        }
+    }
+    
+    func request<T: Decodable>(_ endpoint: Endpoint) async throws(NetworkError) -> ResponseDTO<T> {
+        do {
+            switch endpoint.task {
+            case .uploadMultipart(let multipartFormData):
+                return try await session.upload(multipartFormData: multipartFormData, with: endpoint)
+                    .validate()
+                    .serializingDecodable(ResponseDTO<T>.self, decoder: decoder)
+                    .value
+            default:
+                return try await session.request(endpoint)
+                    .validate()
+                    .serializingDecodable(ResponseDTO<T>.self, decoder: decoder)
+                    .value
+            }
+        } catch let error as AFError {
+            throw mapError(error)
+        } catch {
+            throw NetworkError.decodingFailed(underlyingError: error)
+        }
+    }
+}
+
+// MARK: - Private Methods
+private extension NetworkService {
+    func mapError(_ error: AFError) -> NetworkError {
+        if error.isInvalidURLError {
+            return .invalidRequest(.invalidURL(error.url?.absoluteString ?? "Unknown URL"))
+        }
+        
+        switch error {
+        case .parameterEncodingFailed(let reason):
+            switch reason {
+            case .jsonEncodingFailed(let error):
+                return .invalidRequest(.jsonBodyEncodingFailed(underlyingError: error))
+            default:
+                return .invalidRequest(.parameterEncodingFailed(underlyingError: error))
+            }
+            
+        case .multipartEncodingFailed:
+            return .invalidRequest(.multipartEncodingFailed(underlyingError: error))
+            
+        case .responseValidationFailed(let reason):
+            switch reason {
+            case .unacceptableStatusCode(let code):
+                let statusCodeError = StatusCodeError(statusCode: code)
+                return .invalidResponse(.unacceptableStatusCode(statusCodeError))
+            default:
+                return .invalidResponse(.noResponse)
+            }
+            
+        case .responseSerializationFailed:
+            return .decodingFailed(underlyingError: error)
+            
+        case .sessionTaskFailed(error: let underlyingError):
+            if let urlError = underlyingError as? URLError {
+                switch urlError.code {
+                case .timedOut:
+                    return .invalidResponse(.timeout(underlyingError: urlError))
+                case .notConnectedToInternet:
+                    return .invalidRequest(.networkUnavailable)
+                default:
+                    return .unknown(underlyingError: urlError)
+                }
+            }
+            return .unknown(underlyingError: underlyingError)
+            
+        default:
+            return .unknown(underlyingError: error)
+        }
+    }
+}
