e49c4834a65515f9d16a0843f17795dd9dadbdbe
JinUng41 <ingjwjw@naver.com>
2025-09-22 01:15:08 +0900

    [Feat] #21 - NetworkInterceptor 구현
    - 추후 이뤄져야 할 작업들에 대해서 TODO 주석을 남김.

---
+++ Projects/Network/Sources/NetworkInterceptor.swift
@@ -0,0 +1,34 @@
+//
+//  NetworkInterceptor.swift
+//  Network
+//
+//  Created by 김진웅 on 9/22/25.
+//
+
+import Foundation
+import Alamofire
+
+final class NetworkInterceptor: RequestInterceptor {
+    // TODO: 토큰 관리 객체 주입
+    
+    func adapt(
+        _ urlRequest: URLRequest,
+        for session: Session,
+        completion: @escaping (Result<URLRequest, Error>) -> Void
+    ) {
+        // TODO: 토큰 삽입
+        
+        completion(.success(urlRequest))
+    }
+    
+    func retry(
+        _ request: Request,
+        for session: Session,
+        dueTo error: Error,
+        completion: @escaping (RetryResult) -> Void
+    ) {
+        // TODO: 토큰 재발급 로직 구현
+        
+        completion(.doNotRetry)
+    }
+}
