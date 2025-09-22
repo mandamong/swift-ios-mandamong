362ac80b6acd441f1d0c26d6956e337b4ab178e8
YunSwain <destap@naver.com>
2025-09-22 22:01:24 +0900

    [Chore] #23 - 멤버와이즈 수정자 이용

--- Projects/Mandarat/MandaratDomain/Sources/Entities/Mandarat.swift
+++ Projects/Mandarat/MandaratDomain/Sources/Entities/Mandarat.swift
@@ -12,12 +12,6 @@ struct ActionItem: Identifiable {
     let id: UInt
     var action: String
     var isCompleted: Bool
-    
-    init(id: UInt, action: String = String(), isCompleted: Bool = false) {
-        self.id = id
-        self.action = action
-        self.isCompleted = isCompleted
-    }
 }
 
 /// 핵심 주제별 목표
@@ -25,12 +19,6 @@ struct Objective: Identifiable {
     let id:  UInt
     var content: String
     var actionItems: [ActionItem]
-    
-    init(id: UInt, content: String = String(), actionItems: [ActionItem] = []) {
-        self.id = id
-        self.content = content
-        self.actionItems = actionItems
-    }
 }
 
 /// 핵심 주제
@@ -38,27 +26,13 @@ struct Subject: Identifiable {
     let id: UInt
     var content: String
     var isCompleted: Bool
-    
-    init(id: UInt, content: String = String(), isCompleted: Bool = false) {
-        self.id = id
-        self.content = content
-        self.isCompleted = isCompleted
-    }
 }
 
 /// 만다라트 차트
 struct Mandarat: Identifiable {
     let id: UInt
-    var title: String
+    var title: String = ""
     var subject: Subject
     var objectives: [Objective]
-    var createdAt: Date
-    
-    init(id: UInt, title: String, subject: Subject, objectives: [Objective], createdAt: Date = .now) {
-        self.id = id
-        self.title = title
-        self.subject = subject
-        self.objectives = objectives
-        self.createdAt = createdAt
-    }
+    let createdAt: Date = .now
 }
