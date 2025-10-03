eff65f8c46bc77b9e061c662dcf6eb50ee348e00
SwainYun <destap@naver.com>
2025-10-03 15:10:46 +0900

    [Setting] #31 - 불필요 의존성 제거, LaunchScreen 설정 추가

--- Projects/App/Project.swift
+++ Projects/App/Project.swift
@@ -9,20 +9,15 @@ let project = Project(
             product: .app,
             bundleId: "com.mandamong.app",
             deploymentTargets: .iOS("17.0"),
-            infoPlist: .default,
+            infoPlist: .extendingDefault(with: [
+                "UILaunchScreen": .dictionary([
+                    "UILaunchScreenIsTranslucent": .boolean(false)
+                ])
+            ]),
             sources: ["Sources/**"],
             dependencies: [
                 .project(target: "LoginFeature", path: "../Login/LoginFeature"),
-
                 .project(target: "MandaratFeature", path: "../Mandarat/MandaratFeature"),
-
-                .project(target: "DesignSystem", path: "../Shared"),
-                .project(target: "Utils", path: "../Shared"),
-
-                .project(target: "Network", path: "../Network"),
-                .project(target: "LocalDB", path: "../LocalDB"),
-
-                .external(name: "ComposableArchitecture")
             ]
         )
     ]
