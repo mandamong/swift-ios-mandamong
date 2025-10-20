28587ffb1fee6e09795e38f1440a142382ea6fe1
SwainYun <destap@naver.com>
2025-10-20 22:24:46 +0900

    [Feat] #46 - 만다라트 관련 AppStorageKey 정의

---
+++ Projects/Mandarat/MandaratDomain/Sources/AppStorageKey.swift
@@ -0,0 +1,13 @@
+//
+//  AppStorageKey.swift
+//  MandaratDomain
+//
+//  Created by SwainYun on 10/20/25.
+//
+
+import Foundation
+
+/// AppStorage CRUD에 사용되는 키의 이름 공간
+public struct AppStorageKey {
+    public static let mandaratPresentationMode: String = "MandaratPresentationMode"
+}
