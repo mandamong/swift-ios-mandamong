104aa237a2aef86e2f6e0b391add0ce88215844f
SwainYun <destap@naver.com>
2025-10-22 22:04:42 +0900

    [Refactor] #48 - 만다라트 목록 화면에 만다라트 삭제 기능 추가

--- Projects/Mandarat/MandaratFeature/Sources/MandaratHome/MandaratHomeFeature.swift
+++ Projects/Mandarat/MandaratFeature/Sources/MandaratHome/MandaratHomeFeature.swift
@@ -12,14 +12,18 @@ import MandaratDomain
 struct MandaratHomeFeature {
     @ObservableState
     struct State: Equatable {
-        var mandarats: IdentifiedArrayOf<Mandarat> = IdentifiedArray(uniqueElements: Mandarat.mocks)
+        var mandarats: IdentifiedArrayOf<Mandarat>
         var path: StackState<Path.State> = .init()
+        
+        init(mandarats: IdentifiedArrayOf<Mandarat> = .init(uniqueElements: Mandarat.mocks)) {
+            self.mandarats = mandarats
+        }
     }
     
     @CasePathable
     enum Action: BindableAction {
         enum ViewAction {
-            
+            case swipeToDelete(Mandarat.ID)
         }
         
         case view(ViewAction)
@@ -32,10 +36,19 @@ struct MandaratHomeFeature {
         
         Reduce { state, action in
             switch action {
+            case let .view(.swipeToDelete(id)):
+                state.mandarats.remove(id: id)
+                return .none
+                
             case .view:
                 return .none
                 
             case .binding:
+                return .none
+                
+            case let .path(.element(id: pathID, action: .mandaratDetailFeatureAction(.delegate(.deleteMandarat(mandaratID))))):
+                state.mandarats.remove(id: mandaratID)
+                state.path.pop(from: pathID)
                 return .none
                 
             case .path:
